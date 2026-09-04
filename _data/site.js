bc1ef8f48d06b241e185eb8b95df01b7294f3f89
limpiezascastellon <correo@limpiezascastellon.es>
2026-09-04 18:39:51 +0200

    Add files via upload

---
+++ _data/site.js
@@ -0,0 +1,10 @@
+module.exports = {
+  nombre: "Limpiezas Castellón",
+  claim: "La limpieza nos importa",
+  telefono: "+34 964 900 046",
+  telefonoLink: "+34964900046",
+  email: "correo@limpiezascastellon.es",
+  direccion: "C/ Enmedio, 22 6º · 12001 Castellón",
+  url: "https://www.limpiezascastellon.es",
+  whatsapp: "https://wa.me/34964900046",
+};
